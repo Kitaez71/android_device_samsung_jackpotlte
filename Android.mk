4271083848a09efa3ad1dd0e436d2cc78b2f5eb3
Kitaez71 <Kitaez7142@gmail.com>
2020-07-24 18:21:46 +0300

    3 commit

--- Android.mk
+++ Android.mk
@@ -1,6 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),jackpotlte)
+
+ifneq ($(filter jackpotlte,$(TARGET_DEVICE)),)
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
 endif
